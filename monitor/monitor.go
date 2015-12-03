8cf7f063b48d6de1ffe9770d75c2674622807329
Yim <yiyan.lu@me.com>
2015-12-03 09:51:15 +0800

    add monitor and config

---
+++ monitor/monitor.go
@@ -0,0 +1,58 @@
+package monitor
+
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var Monitor = monitor{}
+
+type User struct {
+	LiveCount int64 // use atomic
+	RoomName  string
+	HostName  string
+}
+
+type monitor struct {
+	hostName       string
+	roomName       string
+	liveCount      int64
+	trafficDown    int64
+	avgTrafficDown int64
+	trafficUp      int64
+	avgTrafficUp   int64
+	reportTime     time.Duration
+	sync.RWMutex
+}
+
+func (m *monitor) SetName(host, room string) {
+	m.Lock()
+	m.hostName = host
+	m.roomName = room
+	m.Unlock()
+}
+
+func (m *monitor) GetTempInfo() *User {
+	return &User{
+		LiveCount: atomic.AddInt64(&m.liveCount, 0),
+		RoomName:  m.roomName,
+		HostName:  m.hostName,
+	}
+}
+
+func (m *monitor) UserLogin() {
+	atomic.AddInt64(&m.liveCount, 1)
+}
+
+func (m *monitor) UserLogout() {
+	atomic.AddInt64(&m.liveCount, -1)
+}
+
+func (m *monitor) AddUpTraffic(t int64) {
+	atomic.AddInt64(&m.trafficUp, t)
+}
+
+func (m *monitor) AddDownTraffic(t int64) {
+	atomic.AddInt64(&m.trafficDown, t)
+}
