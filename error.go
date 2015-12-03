febdee9b741fcf366c7387de83735612208e4843
Yim <yiyan.lu@me.com>
2015-12-03 11:39:06 +0800

    add fb team tag

--- error.go
+++ error.go
@@ -1,3 +1,13 @@
+// Copyright © 2015 FlexibleBroadband Team.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//	      ___ _           _ _     _
+//	     / __\ | _____  _(_) |__ | | ___
+//	    / _\ | |/ _ \ \/ / | '_ \| |/ _ \
+//	   / /   | |  __/>  <| | |_) | |  __/
+//	   \/    |_|\___/_/\_\_|_.__/|_|\___|
+
 package main
 
 import "fmt"
